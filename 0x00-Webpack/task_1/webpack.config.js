79df5358538e1a8d91c612dca67e47129fac440e
yusrA312 <yusra2yahya@gmail.com>
2024-07-22 04:06:01 -0700

    done

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    mode: 'production',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public")
+      }
+}
