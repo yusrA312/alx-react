31e314444ae34626dbc3fa2c1cafbae2f8ed5cc0
yusrA312 <yusra2yahya@gmail.com>
2024-08-14 05:03:09 -0700

    done

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,24 +1,17 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(object) {
-  const seq = Seq(object);
+const printBestStudents = (grades) => {
+  // Convert the object to a Seq of key-value pairs
+  const studentsSeq = Seq(grades);
 
-  const filtered = seq.filter((student) => {
-    student.firstName.charAt(0).toUpperCase();
-    return student.score > 70;
-  });
+  const bestStudents = studentsSeq.filter(student => student.score >= 70);
 
-  function capFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  const formattedStudents = bestStudents.map(student => ({
+    ...student,
+    firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+    lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+  }));
 
-  const JSObject = filtered.toJS();
+  console.log(formattedStudents.toObject());
+};
 
-  Object.keys(JSObject).map((key) => {
-    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
-    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
-    return JSObject[key];
-  });
-
-  console.log(JSObject);
-}
